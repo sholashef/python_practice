9eb29754964b80024f91125b188d6bc93c6d5b38
Oluwasola <113778568+sholashef@users.noreply.github.com>
2023-09-17 20:05:16 +0100

    Add files via upload
    program that takes input from the user and displays that
    input back in 1. lower case 2. upper case 3. Shows the number of characters in the input

---
+++ Practice 1.py
@@ -0,0 +1,31 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> talk = input("whats happening ? ")
+whats happening ? I love Yam
+>>> print(talk)
+I love Yam
+>>> talk_loud = talk.upper()
+>>> print(talk_loud)
+I LOVE YAM
+>>> talk_less = talk.lower()
+>>> print(talk_less)
+i love yam
+>>> 
+>>> prompt = ("say something please ? ")
+>>> user_message = input(prompt)
+say something please ? I love Allah, SWT.
+>>> say_loud = user_message.upper()
+>>> print(say_loud)
+I LOVE ALLAH, SWT.
+>>> 
+>>> 
+>>> user_word = input("say something please ? ")
+say something please ? I love This Job
+>>> say_high = user_word.upper()
+>>> say_low = user_word.lower()
+>>> print(say_high)
+I LOVE THIS JOB
+>>> print(say_low)
+i love this job
+>>> print(len(user_word))
+15
